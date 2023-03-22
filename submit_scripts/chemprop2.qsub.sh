39bb6ebe77250895ecdf253e5ef7808ccc93476f
judewells <judewells@hotmail.com>
2023-03-22 21:28:09 +0000

    add submit scripts

---
+++ submit_scripts/chemprop2.qsub.sh
@@ -0,0 +1,24 @@
+#$ -l tmem=8G
+#$ -l h_vmem=8G
+#chemprop approx 0.25s per ligand 
+#$ -l h_rt=00:45:00
+#These are optional flags but you probably want them in all jobs
+
+#$ -S /bin/bash
+#$ -j y
+#$ -N REAL2
+#$ -wd /SAN/orengolab/nsp13/enamine_cluster 
+#second number should n mols in enamine file / batch size
+#$ -t 5001-20100
+#The code you want to run now goes here.
+
+
+date
+
+#Python 3.8.3 Source
+export PATH=/share/apps/python-3.8.5-shared/bin:$PATH
+export LD_LIBRARY_PATH=/share/apps/python-3.8.5-shared/lib:$LD_LIBRARY_PATH
+source /share/apps/source_files/python/python-3.8.5.source
+python3 --version
+bash predict_batch2.sh $SGE_TASK_ID > /dev/null
+date
