0927004f4a0b437c3072fcc9187e2b219ceda48c
judewells <judewells@hotmail.com>
2023-07-21 13:19:41 +0100

    cluster commit fix savepath

--- predict_batch.sh
+++ predict_batch.sh
@@ -1,4 +1,4 @@
-BATCHSIZE=20000
+BATCHSIZE=40000
 INDEX=${1}
 FILE=${2}
 MODEL_DIR=${3}
@@ -7,6 +7,6 @@ EXPERIMENT_NAME=${5}
 START=$(($((INDEX * BATCHSIZE)) + 1))
 END=$(($START + BATCHSIZE))
 sed -n "$START,${END}p;$(($END + 1))q" $FILE > smiles_file.txt
-python3 run_chemprop.py smiles_file.txt $START $FILE $MODEL_DIR $THRESHOLD $EXPERIMENT_NAME
+python run_chemprop.py smiles_file.txt $START $FILE $MODEL_DIR $THRESHOLD $EXPERIMENT_NAME
 echo nice_jobdone
 rm REAL.o*
