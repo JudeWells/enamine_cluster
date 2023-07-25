2d79417a1539518e08c89ca4681c000413628a6a
judewells <judewells@hotmail.com>
2023-07-25 13:26:43 +0100

    update job params

--- submit_scripts/chemprop_cache4.qsub.sh
+++ submit_scripts/chemprop_cache4.qsub.sh
@@ -6,21 +6,23 @@
 
 #$ -S /bin/bash
 #$ -j y
-#$ -N mini_real
+#$ -N c4_classifier
 #$ -wd /SAN/orengolab/nsp13/enamine_cluster 
 #second number should n mols in enamine file / batch size
-#$ -t 1-2 #10050
+#$ -t 1-1610 #10050
+#$ -o /SAN/orengolab/nsp13/enamine_cluster/logs/
 #The code you want to run now goes here.
+
 WORKING_DIR=/SAN/orengolab/nsp13/enamine_cluster
 cd $WORKING_DIR
 FILE=${WORKING_DIR}/Enamine_REAL_HAC_22_23_402M_CXSMILES.cxsmiles
 MODEL_DIR=${WORKING_DIR}/chemprop_new_classifier_ens_2
-THRESHOLD=1.0
+THRESHOLD=0.9
 EXPERIMENT_NAME=cache4_classifier2
 echo $EXPERIMENT_NAME
 
 which conda
 conda activate cache3
 which python
-bash predict_batch.sh $SGE_TASK_ID $FILE $MODEL_DIR $THRESHOLD $EXPERIMENT_NAME
+bash predict_batch.sh $SGE_TASK_ID $FILE $MODEL_DIR $THRESHOLD $EXPERIMENT_NAME 
 date
