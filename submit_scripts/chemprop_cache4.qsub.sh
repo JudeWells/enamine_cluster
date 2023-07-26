24e4997a066878e095dac8f4dfc20db29e8871bf
judewells <judewells@hotmail.com>
2023-07-26 14:54:51 +0100

    update submit script

--- submit_scripts/chemprop_cache4.qsub.sh
+++ submit_scripts/chemprop_cache4.qsub.sh
@@ -1,7 +1,7 @@
 #$ -l tmem=16G
 #$ -l h_vmem=16G
 #chemprop approx 0.25s per ligand 
-#$ -l h_rt=01:45:00
+#$ -l h_rt=11:45:00
 #These are optional flags but you probably want them in all jobs
 
 #$ -S /bin/bash
@@ -9,7 +9,7 @@
 #$ -N c4_classifier
 #$ -wd /SAN/orengolab/nsp13/enamine_cluster 
 #second number should n mols in enamine file / batch size
-#$ -t 1-2 # 1610 
+#$ -t 1-4023
 #$ -o /SAN/orengolab/nsp13/enamine_cluster/logs/
 #The code you want to run now goes here.
 
@@ -18,8 +18,8 @@ cd $WORKING_DIR
 FILE=${WORKING_DIR}/Enamine_REAL_HAC_22_23_402M_CXSMILES.cxsmiles #402282236
 MODEL_DIR=${WORKING_DIR}/chemprop_new_classifier_ens_2
 THRESHOLD=0.9
-EXPERIMENT_NAME=cache4_classifier2
-BATCH_SIZE=250000
+EXPERIMENT_NAME=c4_22_23
+BATCH_SIZE=100000
 echo $EXPERIMENT_NAME
 
 which conda
